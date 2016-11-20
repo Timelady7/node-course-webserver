a76c3b4330eef0a1e174989ba6b8da09e1f9a093
Timelady7 <timelady47@gmail.com>
2016-11-20 18:04:32 +0100

    project page

--- server.js
+++ server.js
@@ -42,6 +42,12 @@ app.get('/about', (request, response) => {
     });
 });
 
+app.get('/projects', (request, response) => {
+    response.render('projects.hbs', {
+        pageTitle: 'My Portfolio',
+    });
+});
+
 app.get('/bad', (request, response) => {
     response.send({
         errorMessage: 'Error handling message'
